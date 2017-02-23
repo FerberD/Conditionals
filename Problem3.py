0243f8e7c3f5333e50799436ebfac25997af1d59
Drake <dferber@sjnma.org>
2017-02-23 10:29:28 -0600

    progress on Problem 3

---
+++ Problem3.py
@@ -0,0 +1,22 @@
+
+number = float(raw_input("number"))
+mood = str(raw_input("how is your day going"))
+
+if(4 < number and number < 26):
+    print("In range")
+
+
+print(number < 5)
+print(number > 25)
+
+print(mood == "good")
+print(mood == "Good")
+print(4 < number)
+print(26 > number)
+
+
+
+
+if((mood == "Good" or mood == "good") and 5.0 > number and number > 25.0):
+	print(":-o")
+
